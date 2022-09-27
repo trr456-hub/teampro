421c2725f2f8132d62d94f4cd5b4ed4f34060eb8
seokgeun <trr456@naver.com>
2022-09-27 17:30:41 +0900

    DB Test

---
+++ teampro/dataBase_Source/woori.sql
@@ -0,0 +1,57 @@
+-- MariaDB dump 10.19  Distrib 10.5.11-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: woori
+-- ------------------------------------------------------
+-- Server version	10.5.11-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `sungjuk`
+--
+
+DROP TABLE IF EXISTS `sungjuk`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sungjuk` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` char(10) DEFAULT NULL,
+  `kor` int(11) DEFAULT NULL,
+  `eng` int(11) DEFAULT NULL,
+  `mat` int(11) DEFAULT NULL,
+  `writeday` date DEFAULT NULL,
+  `iphak` date DEFAULT NULL,
+  `juso` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sungjuk`
+--
+
+LOCK TABLES `sungjuk` WRITE;
+/*!40000 ALTER TABLE `sungjuk` DISABLE KEYS */;
+INSERT INTO `sungjuk` VALUES (3,'홍길동',90,90,80,'2022-09-22','2021-03-01','서울'),(4,'배트맨',90,60,70,'2022-09-22','2022-03-01','제주도'),(5,'슈퍼맨',80,70,70,'2022-09-22','2022-03-01','부산'),(6,'원더우먼',70,90,90,'2022-09-22','2021-03-01','목포'),(7,'뽀로로',50,80,80,'2022-09-22','2022-03-01','남극'),(8,'나그네',60,40,40,'2022-09-22','2022-03-01','사모아'),(9,'도깨비',50,90,90,'2022-09-22','2020-03-01',NULL);
+/*!40000 ALTER TABLE `sungjuk` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-09-26 12:13:39
